5e0cb17f1dfd164f56b4c45a0fe5712fb1db44c2
Youness Aloui <kakaroto@users.sourceforge.net>
2007-09-13 17:01:30 +0000

    Fixes login button

--- amsn2/gui/efl.py
+++ amsn2/gui/efl.py
@@ -99,8 +99,6 @@ class aMSNLoginWindow_EFL(aMSNLoginWindow):
         self._evas.data["login_window"] = self._edje
         
         self._edje.on_key_down_add(self.__on_key_down)
-        self._edje.signal_callback_add("signin", "*", self.__signin_cb)
-
 
         self.password = etk.Entry()
         embed = etk.Embed(self._evas.evas)
@@ -121,6 +119,17 @@ class aMSNLoginWindow_EFL(aMSNLoginWindow):
         embed.show_all()
         self._edje.part_swallow("login_screen.username", embed.object)
         
+        if self._edje.part_exists("login_screen.signin"):
+           self.signin = etk.Button()
+           embed = etk.Embed(self._evas.evas)
+           embed.add(self.signin)
+           embed.show_all()
+           self._edje.part_swallow("login_screen.signin", embed.object)
+           self.signin.label = "Sign in"
+           self.signin.connect("clicked", self.__signin_button_cb)
+        else:
+           self._edje.signal_callback_add("signin", "*", self.__signin_cb)
+
         self._edje.focus = True
 
         # We start with no profile set up, we let the Core set our starting profile
@@ -139,7 +148,7 @@ class aMSNLoginWindow_EFL(aMSNLoginWindow):
             self.password.text = self.current_profile.password
 
 
-    def signin(self):
+    def login(self):
         # TODO : get/set the username/password and other options from the login screen
         self.current_profile.username = self.username.text
         self.current_profile.email = self.username.text
@@ -185,7 +194,11 @@ class aMSNLoginWindow_EFL(aMSNLoginWindow):
             ecore.main_loop_quit()
 
     def __signin_cb(self, edje_obj, signal, source):
-        self.signin()
+        self.login()
+
+    def __signin_button_cb(self, button):
+        print "clicked %s - %s" % (self, button)
+        self.login()
 
 
 
