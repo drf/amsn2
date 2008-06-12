42c3c8dc753d3723397d0e87c506c759c423177e
Youness Aloui <kakaroto@users.sourceforge.net>
2008-06-12 15:09:41 +0000

    Creating amsn2 directory in SVN w

--- core/profilemanager.tcl
+++
@@ -1,7 +0,0 @@
-# this should deal with profiles, locking/releasing/creating/etc.. profiles
-# all the config for a profile should be stored in a 'Profile' object which can be accessed from this ProfileManager. It should replace [::config::g/setKey *] calls...
-
-
-snit::type ProfileManager {
-
-}
--- protocols/pkgIndex.tcl
+++
@@ -1,5 +0,0 @@
-# Tcl package index file, version 1.0
-
-package ifneeded aMSN2_Protocols 0.1 "[list lappend ::auto_path $dir]; \
-                                      [list source [file join $dir protocolmanager.tcl]]; \
-                                      [list package provide aMSN2_Protocols 0.1]"
