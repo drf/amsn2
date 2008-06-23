db57538a34c2224fcac590ab23516c78d23f9d15
Giovanni Gola <doppiag@gmail.com>
2008-06-23 13:42:19 +0000

    * Added aMSNProfilePlugin class, I'll do the same with smileys & so on  * Implemented saving and loading profiles to disk  * Some more stuff

--- amsn2/core/profile.py
+++ amsn2/core/profile.py
@@ -1,3 +1,9 @@
+
+import os
+import xml.etree.ElementTree
+import xml.parsers.expat
+import __builtin__
+
 class aMSNProfileConfiguration(object):
     def __init__(self, profile):
         self._profile = profile
@@ -5,6 +11,22 @@ class aMSNProfileConfiguration(object):
                        "ns_port":1863,
                        }
         
+    def getKey(self, key, default = None):
+        try:
+            return self.config[key]
+        except KeyError:
+            return default
+        
+    def setKey(self, key, value):
+        self.config[key] = value
+
+class aMSNProfilePlugin(object):
+    """ aMSNProfilePlugin : A plugin object for profiles."""
+    def __init__(self, profile, config_dict):
+        """ config_dict must be a dictionary of configurations ("option": value, ...) """
+        self._profile = profile
+        self.config = config_dict
+
     def getKey(self, key, default = None):
         try:
             return self.config[key]
@@ -26,6 +48,10 @@ class aMSNProfile(object):
         self.password = None
         self.account = None
         self.config = aMSNProfileConfiguration(self)
+        ###self.plugins must be a dictionary like {"Plugin1_name":aMSNProfilePlugin(self,{"option1":123}),
+        ###                                        "Plugin2_name":aMSNProfilePlugin(self,{"option1":"sdda","option2":345})
+        ###                                        }
+        self.plugins = {}
     
     def isLocked(self):
         """ Returns whether the profile is locked or not"""
@@ -45,18 +71,28 @@ class aMSNProfile(object):
     def setConfigKey(self, key, value):
         return self.config.setKey(key, value)
 
+    def getPluginKey(self, plugin_name, key, default = None):
+        return self.plugins.getKey(plugin_name, key, default)
+
+    def setPluginKey(self, plugin_name, key, value):
+        return self.plugins.setKey(plugin_name, key, value)
+
 class aMSNProfileManager(object):
     """ aMSNProfileManager : The profile manager that takes care of storing
     and retreiving all the profiles for our users.
     """
     def __init__(self):
-        # TODO read profiles fro disk
+        if os.name == "posix":
+            self._profiles_dir = os.path.join(os.environ['HOME'], ".amsn2", "profiles")
+        elif os.name == "nt":
+            self._profiles_dir = os.path.join(os.environ['USERPROFILE'], "amsn2", "profiles")
         self.profiles = {}
+        self.loadAllProfiles()
 
     def profileExists(self, email):
         """ Checks whether a profile exists """
         return self.getProfile(email) is not None
-
+    
     def getProfile(self, email):
         """ Get a profile object by email """
         try:
@@ -78,19 +114,172 @@ class aMSNProfileManager(object):
 
     def createProfile(self, email):
         """ Creates a profile and stores it on disk and adds it to the current instance """
-        # TODO : Write to disk info about the current profile
-        return addProfile(email)
+        new_profile = self.addProfile(email)
+        self.saveProfile(new_profile)
+        return new_profile
 
     def removeProfile(self, profile):
         """ Removes a profile from the current instance of aMSN """
         if self.profileExists(profile.email):
             del self.profiles[profile.email]
         
-
     def deleteProfile(self, profile):
         """ Removes a profile from the current instance of aMSN and deletes it from disk """
-        # TODO : remove from disk
-        return removeProfile(self, profile)
+        profile_dir_path = os.path.join(os.environ['HOME'], ".amsn2", "profiles", profile.email)
         
+        for root, dirs, files in os.walk(profile_dir_path, topdown=False):
+            for name in files:
+                os.remove(os.path.join(root, name))
+            for name in dirs:
+                os.rmdir(os.path.join(root, name))
+        try:
+            os.rmdir(profile_dir_path)
+        except:
+            pass
 
+        return self.removeProfile(profile)
+    
+    def saveProfile(self, profile):
+        """ Stores a profile on disk """        
+        config = profile.config.config
+            
+        config_section = dictToElement("Configurations", config)
         
+        settings = {"email":profile.email,
+                    "username":profile.username,
+                    "alias":profile.alias,
+                    "password":profile.password,
+                    "account":profile.account
+                    }
+        
+        if profile.password == None :
+            settings["password"] = ""
+        if profile.account == None :
+            settings["account"] = ""
+        
+        settings_section = dictToElement("Settings", settings)
+        
+        plugins = profile.plugins
+        
+        plugins_section = xml.etree.ElementTree.Element("Plugins")
+        
+        for plugin_name, plugin in plugins.iteritems():
+            plugin_section = dictToElement(plugin_name, plugin.config)
+            plugins_section.append(plugin_section)
+        
+        root_section = xml.etree.ElementTree.Element("aMSNProfile")
+        
+        settings_section.append(config_section)
+        settings_section.append(plugins_section)
+        root_section.append(settings_section)
+        
+        xml_tree = xml.etree.ElementTree.ElementTree(root_section)
+        
+        profile_dir = os.path.join(self._profiles_dir, profile.email)
+        
+        try :
+            os.makedirs(profile_dir, 0777)
+            os.makedirs(os.path.join(profile_dir, "smileys"), 0777)
+            os.makedirs(os.path.join(profile_dir, "displaypics"), 0777)
+            ## Other directories here
+        except :
+            pass
+        
+        profile_path = os.path.join(self._profiles_dir, profile.email, "settings.xml")
+        profile_file = file(profile_path, "w")
+        xml_tree.write(profile_file)
+        profile_file.close()
+    
+    def loadAllProfiles(self):
+        """ Loads all profiles from disk """  
+        try :
+            os.makedirs(self._profiles_dir, 0777)
+        except :
+            pass
+        
+        for root, dirs, files in os.walk(self._profiles_dir):
+            profiles_dirs = dirs
+            ### To be extended
+            break
+
+        for profile_dir in profiles_dirs :
+            profile_file_path = os.path.join(self._profiles_dir, profile_dir, "settings.xml")
+            root_tree = xml.etree.ElementTree.parse(profile_file_path)
+            settings = root_tree.find("Settings")
+            settings_tree = xml.etree.ElementTree.ElementTree(settings)
+            configs = settings_tree.find("Configurations")
+            settings.remove(configs)
+            plugins = root_tree.find("Plugins")
+            settings.remove(plugins)
+            
+            settings_dict = elementToDict(settings) 
+            profile = aMSNProfile(settings_dict['email'])
+            profile.username = settings_dict['username']
+            profile.alias = settings_dict['alias']
+            profile.password = settings_dict['password']
+            profile.account = settings_dict['account']
+            profile.config = aMSNProfileConfiguration(profile)
+
+            if profile.password == "" :
+                profile.password = None
+            if profile.account == "" :
+                profile.account = None
+
+            configs_dict = elementToDict(configs)
+            profile.config.config = configs_dict
+
+            plugins_dict = {}
+            for plugin_element in plugins:
+                plugins_dict[plugin_element.tag] = elementToDict(plugin_element)
+            
+            profile.plugins = {}
+            for plugin_name, plugin_config_dict in plugins_dict.iteritems():
+                profile.plugins[plugin_name] = aMSNProfilePlugin(profile, plugin_config_dict)
+            
+            self.profiles[profile.email] = profile
+
+def elementToDict(element):
+    """ Converts an XML Element into a proper profile dictionary """
+    def dictToTuple(name, dict):
+        """ Converts a dictionary returned by expat XML parser into a proper tuple and adds it to a list ready for dict() """
+        key = dict['name']
+        type = dict['type']
+        value = dict['value']
+        
+        config_pair = (key,eval(type)(value))
+        
+        config_pair_list.append(config_pair)
+    
+    config_pair_list = []
+    
+    for entry in element :
+        entry_str = xml.etree.ElementTree.tostring(entry)
+        
+        parser=xml.parsers.expat.ParserCreate()
+        parser.StartElementHandler = dictToTuple
+        parser.Parse(entry_str, 1)
+        del parser
+        
+    config_dict = dict(config_pair_list)
+    
+    return config_dict
+
+def dictToElement(name, dict):
+    """ Converts a dictionary into a proper XML Element with tag 'name' """
+    keys=[]
+    types=[]
+    values=[]
+    
+    root_element = xml.etree.ElementTree.Element(name)
+    
+    for key, value in dict.iteritems() :
+        keys.append(key)
+        values.append(str(value))
+        type_obj = __builtin__.type(value)
+        types.append(str(type_obj)[7:-2])
+    
+    for key, type, value in zip(keys, types, values) :
+        element = xml.etree.ElementTree.Element("entry", {"name":key, "type":type, "value":value})
+        root_element.append(element)
+    
+    return root_element
