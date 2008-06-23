4808d6989db9f304d48deeee3b0d837bf31bb286
Dario Freddi <drf54321@gmail.com>
2008-06-23 20:22:35 +0000

    Refining interface, GUI shouldn't care about who sent what, but just about displaying stuff.

--- amsn2/gui/base/chat_window.py
+++ amsn2/gui/base/chat_window.py
@@ -11,14 +11,21 @@ class aMSNChatWindow(object):
         """ Hide the chat window """
         raise NotImplementedError
 
-    def messageSent(self, contact,  message):
-        """ This method will be called to notify the chat window that a new message was sent from contact.
-        Message is a stringview, so it should be treated correctly.
+    def messageSent(self,  message):
+        """ This method will be called to notify the chat window that a new message was sent and should be displayed.
+        Message is a stringview, so the GUI doesn't care who sent it.
         """
         raise NotImplementedError
     
-    def nudgeSent(self, contact):
-        """ This method will be called to notify the chat window that a nudge was sent from contact.
+    def nudgeSent(self):
+        """ This method will be called to notify the chat window that a nudge was sent.
+        """
+        raise NotImplementedError
+        
+    
+    def userTyping(self,  message):
+        """ This method notifies the window a user is typing. Message is the message that should be
+        displayed, and is a stringview
         """
         raise NotImplementedError
 
