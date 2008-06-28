e9316230c8226a642ad09f29ca1a54920970571d
Giovanni Gola <doppiag@gmail.com>
2008-06-28 17:00:55 +0000

    * aMSNProfilesList represents profiles.xml (partially implemented: load/save works, set/get don't)  * def of elementToDict and dictToElement moved in the library and added support for bool values  * Other minor changes

--- amsn2/core/profile.py
+++ amsn2/core/profile.py
@@ -4,6 +4,63 @@ import xml.etree.ElementTree
 import xml.parsers.expat
 import __builtin__
 
+class aMSNProfilesList(object):
+    """ aMSNProfilesList : Object representing profiles.xml """
+    def __init__(self, profiles_dir):
+        self.path = os.path.join(profiles_dir, "profiles.xml")
+        self.updateProfilesList()
+    
+    def updateProfilesList(self):
+        """ Reads the content of profiles.xml """
+        if os.access(self.path, os.F_OK):
+            profiles_file = file(self.path, "r")
+            
+            self.tree = xml.etree.ElementTree.ElementTree(file=profiles_file)
+            
+            profiles_file.close()
+            
+            self.root_element = self.tree.getroot()
+        else :
+            self.root_element = xml.etree.ElementTree.Element("aMSNProfilesList")
+            
+            self.tree = xml.etree.ElementTree.ElementTree(element = profiles_element)
+    
+    def setProfileKey(self, profile_name, key, value):
+        pass
+    
+    def getProfileKey(self, profile_name, key):
+        pass
+    
+    def addProfile(self, profile, attributes_dict):
+        """ Adds a profile section in profiles.xml """
+        self.updateProfilesList()
+        profile_element = dictToElement(profile.email.replace("@","_"), attributes_dict)
+        self.root_element.append(profile_element)
+        self.saveProfilesList()
+    
+    def deleteProfile(self, profile):
+        """ Deletes a profile section from profiles.xml """
+        self.updateProfilesList()
+        for profile_element in self.root_element :
+            if profile_element.tag == profile.email.replace("@","_"):
+                self.root_element.remove(profile_element)
+        self.saveProfilesList()
+    
+    def getProfilesNamesList(self):
+        """ Returns a list of all profiles' email addresses """
+        self.updateProfilesList()
+        names_list = []
+        
+        for profile_element in self.root_element:
+            profile_dict = elementToDict(profile_element)
+            names_list.append(profile_dict["email"])
+        
+        return names_list
+
+    def saveProfilesList(self):
+        """ Dumps aMSNProfilesList XML tree into profiles.xml """
+        return self.tree.write(self.path)
+
 class aMSNProfileConfiguration(object):
     def __init__(self, profile):
         self._profile = profile
@@ -42,15 +99,16 @@ class aMSNProfile(object):
     This class will contain all settings relative to a profile
     and will store the protocol and GUI objects    
     """
-    def __init__(self, email):
+    def __init__(self, email, profiles_dir):
         self.email = email
         self.username = self.email
         self.alias = self.email
         self.password = None
         self.account = None
+        self.directory = os.path.join(profiles_dir, self.email)
         self.config = aMSNProfileConfiguration(self)
-        ###self.plugins must be a dictionary like {"Plugin1_name":aMSNProfilePlugin(self,{"option1":123}),
-        ###                                        "Plugin2_name":aMSNProfilePlugin(self,{"option1":"sdda","option2":345})
+        ###self.plugin_configs must be a dictionary like {"Plugin1_name":aMSNPluginConfiguration(self,"Plugin1_name",{"option1":123}),
+        ###                                        "Plugin2_name":aMSNPluginConfiguration(self,"Plugin2_name",{"option1":"sdda","option2":345})
         ###                                        }
         self.plugin_configs = {}
     
@@ -65,6 +123,19 @@ class aMSNProfile(object):
     def unlock(self):
         """ Unlocks a profile to allow other instances to acces it """
         pass
+    
+    def remove_dir(self):
+        """ Removes profile's directory from disk """
+        
+        for root, dirs, files in os.walk(self.directory, topdown=False):
+            for name in files:
+                os.remove(os.path.join(root, name))
+            for name in dirs:
+                os.rmdir(os.path.join(root, name))
+        try:
+            os.rmdir(self.directory)
+        except:
+            pass
 
     def getConfigKey(self, key, default = None):
         return self.config.getKey(key, default)
@@ -90,7 +161,8 @@ class aMSNProfile(object):
             aMSNPluginConfiguration(self, plugin_name, plugin_config_dict)
     
     def removePlugin(self, plugin_name):
-        return self.plugins.pop(plugin_name, None)
+        return self.plugin_configs.pop(plugin_name, None)
+    
 
 class aMSNProfileManager(object):
     """ aMSNProfileManager : The profile manager that takes care of storing
@@ -103,9 +175,17 @@ class aMSNProfileManager(object):
             self._profiles_dir = os.path.join(os.environ['USERPROFILE'], "amsn2")
         else:
             self._profiles_dir = os.path.join(os.curdir, "amsn2")
+            
+        try :
+            os.makedirs(self._profiles_dir, 0777)
+        except :
+            pass
+        
+        self.profiles_list = aMSNProfilesList(self._profiles_dir)
+        
         self.profiles = {}
         self.loadAllProfiles()
-
+        
     def profileExists(self, email):
         """ Checks whether a profile exists """
         return self.getProfile(email) is not None
@@ -124,7 +204,7 @@ class aMSNProfileManager(object):
     def addProfile(self, email):
         """ Adds a profile to the current running instance of aMSN """
         if self.profileExists(email) is False:
-            new_profile = aMSNProfile(email)
+            new_profile = aMSNProfile(email, self._profiles_dir)
             self.profiles[email] = new_profile
         
         return self.getProfile(email)
@@ -132,35 +212,35 @@ class aMSNProfileManager(object):
     def createProfile(self, email):
         """ Creates a profile and stores it on disk and adds it to the current instance """
         new_profile = self.addProfile(email)
+        new_profile_opts = {"email":email, 
+                            "auto_connect":False
+                            } ### Other attributes
+        self.profiles_list.addProfile(new_profile, new_profile_opts)
         self.saveProfile(new_profile)
         return new_profile
 
-    def removeProfile(self, profile):
+    def removeProfile(self, profile, and_delete=False):
         """ Removes a profile from the current instance of aMSN """
         if self.profileExists(profile.email):
             del self.profiles[profile.email]
         
+        self.profiles_list.deleteProfile(profile)
+        
+        if and_delete == True:
+            self.deleteProfile(profile)
+        
     def deleteProfile(self, profile):
         """ Removes a profile from the current instance of aMSN and deletes it from disk """
-        profile_dir_path = os.path.join(self._profiles_dir, profile.email)
-        
-        for root, dirs, files in os.walk(profile_dir_path, topdown=False):
-            for name in files:
-                os.remove(os.path.join(root, name))
-            for name in dirs:
-                os.rmdir(os.path.join(root, name))
-        try:
-            os.rmdir(profile_dir_path)
-        except:
-            pass
+        profile.remove_dir()
 
         return self.removeProfile(profile)
     
     def saveProfile(self, profile):
-        """ Stores a profile on disk """        
+        """ Stores a profile on disk """ 
+        
         config = profile.config.config
             
-        config_section = self.__dictToElement("Configurations", config)
+        config_section = dictToElement("Configurations", config)
         
         settings = {"email":profile.email,
                     "username":profile.username,
@@ -174,14 +254,14 @@ class aMSNProfileManager(object):
         if profile.account == None :
             settings["account"] = ""
         
-        settings_section = self.__dictToElement("Settings", settings)
+        settings_section = dictToElement("Settings", settings)
         
-        plugins = profile.plugins
+        plugins = profile.plugin_configs
         
         plugins_section = xml.etree.ElementTree.Element("Plugins")
         
         for plugin_name, plugin in plugins.iteritems():
-            plugin_section = self.__dictToElement(plugin_name, plugin.config)
+            plugin_section = dictToElement(plugin_name, plugin.config)
             plugins_section.append(plugin_section)
         
         root_section = xml.etree.ElementTree.Element("aMSNProfile")
@@ -198,6 +278,7 @@ class aMSNProfileManager(object):
             os.makedirs(profile_dir, 0777)
             os.makedirs(os.path.join(profile_dir, "smileys"), 0777)
             os.makedirs(os.path.join(profile_dir, "displaypics"), 0777)
+            os.makedirs(os.path.join(profile_dir, "logs"), 0777)
             ## Other directories here
         except :
             pass
@@ -209,18 +290,12 @@ class aMSNProfileManager(object):
     
     def loadAllProfiles(self):
         """ Loads all profiles from disk """  
-        try :
-            os.makedirs(self._profiles_dir, 0777)
-        except :
-            pass
+        profiles_names = self.profiles_list.getProfilesNamesList()
         
-        for root, dirs, files in os.walk(self._profiles_dir):
-            profiles_dirs = dirs
-            ### To be extended
-            break
-
-        for profile_dir in profiles_dirs :
-            profile_file_path = os.path.join(self._profiles_dir, profile_dir, "config.xml")
+        for profile_name in profiles_names :
+            profile_file_path = os.path.join(self._profiles_dir, \
+                                             profile_name, \
+                                             "config.xml")
             
             ### Prepares XML Elements
             root_tree = xml.etree.ElementTree.parse(profile_file_path)
@@ -232,8 +307,8 @@ class aMSNProfileManager(object):
             settings.remove(plugins)
             
             ### Loads Settings
-            settings_dict = self.__elementToDict(settings) 
-            profile = aMSNProfile(settings_dict['email'])
+            settings_dict = elementToDict(settings)
+            profile = aMSNProfile(settings_dict['email'], self._profiles_dir)
             profile.username = settings_dict['username']
             profile.alias = settings_dict['alias']
             profile.password = settings_dict['password']
@@ -246,13 +321,13 @@ class aMSNProfileManager(object):
                 profile.account = None
                 
             ### Loads Configurations
-            configs_dict = self.__elementToDict(configs)
+            configs_dict = elementToDict(configs)
             profile.config.config = configs_dict
             
             ### Loads Plugins
             plugins_dict = {}
             for plugin_element in plugins:
-                plugins_dict[plugin_element.tag] = self.__elementToDict(plugin_element)
+                plugins_dict[plugin_element.tag] = elementToDict(plugin_element)
             
             profile.plugins = {}
             for plugin_name, plugin_config_dict in plugins_dict.iteritems():
@@ -261,50 +336,56 @@ class aMSNProfileManager(object):
             ### Finally loads the Profile
             self.profiles[profile.email] = profile
 
-            
-    def __dictToTuple(self, name, dict):
+
+def elementToDict(element):
+    """ Converts an XML Element into a proper profile dictionary """
+    def dictToTuple(name, dict):
         """ Converts a dictionary returned by expat XML parser into a proper tuple and adds it to a list ready for dict() """
         key = dict['name']
         type = dict['type']
-        value = dict['value']
+        if type == "bool":
+            value = int(dict['value'])
+        else:
+            value = dict['value']
         
         config_pair = (key,eval(type)(value))
         
         config_pair_list.append(config_pair)
-
-    def __elementToDict(self, element):
-        """ Converts an XML Element into a proper profile dictionary """
-    
-        config_pair_list = []
-    
-        for entry in element :
-            entry_str = xml.etree.ElementTree.tostring(entry)
         
-            parser=xml.parsers.expat.ParserCreate()
-            parser.StartElementHandler = self.__dictToTuple
-            parser.Parse(entry_str, 1)
-            del parser
-        
-            config_dict = dict(config_pair_list)
-    
-            return config_dict
+    config_pair_list = []
 
-    def __dictToElement(self, name, dict):
-        """ Converts a dictionary into a proper XML Element with tag 'name' """
-        keys=[]
-        types=[]
-        values=[]
+    for entry in element :
+        entry_str = xml.etree.ElementTree.tostring(entry)
     
-        root_element = xml.etree.ElementTree.Element(name)
+        parser=xml.parsers.expat.ParserCreate()
+        parser.StartElementHandler = dictToTuple
+        parser.Parse(entry_str, 1)
+        del parser
     
-        for key, value in dict.iteritems() :
-            keys.append(key)
+    config_dict = dict(config_pair_list)
+
+    return config_dict
+
+def dictToElement(name, dict):
+    """ Converts a dictionary into a proper XML Element with tag 'name' """
+    keys=[]
+    types=[]
+    values=[]
+
+    root_element = xml.etree.ElementTree.Element(name)
+
+    for key, value in dict.iteritems() :
+        keys.append(key)
+        type = str(__builtin__.type(value))[7:-2]
+        types.append(type)
+        if type == "bool":
+            int_value = int(value)
+            values.append(str(int_value))
+        else:
             values.append(str(value))
-            type_obj = __builtin__.type(value)
-            types.append(str(type_obj)[7:-2])
-    
-        for key, type, value in zip(keys, types, values) :
-            element = xml.etree.ElementTree.Element("entry", {"name":key, "type":type, "value":value})
-            root_element.append(element)
-    
-        return root_element
+
+    for key, type, value in zip(keys, types, values) :
+        element = xml.etree.ElementTree.Element("entry", {"name":key, "type":type, "value":value})
+        root_element.append(element)
+
+    return root_element
