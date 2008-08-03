92babb79810326ec56c3adf23e153706e1f10aad
Boris Faure <billiob@users.sourceforge.net>
2008-08-03 20:07:18 +0000

    simplify the Image class

--- amsn2/gui/base/image.py
+++ amsn2/gui/base/image.py
@@ -5,12 +5,23 @@ class Image(object):
         """Initialize the interface. You should store the reference to the core in here """
         raise NotImplementedError
 
-    def loadFromFile(self, filename):
-        """ This method is used to load an image from a file """
+    def load(self, resource_name, value):
+        """ This method is used to load an image using the name of a resource and a value for that resource
+            resource_name can be :
+                - 'File', value is the filename
+                - 'Skin', value is the skin key
+                - some more :)
+        """
         raise NotImplementedError
 
-    def loadFromResource(self, resource_name):
-        """ This method is used to load an image using the name of a resource """
+    def append(self, resource_name, value):
+        """ This method is used to overlap an image on the current image
+            Have a look at the documentation of the 'load' method for the meanings of 'resource_name' and 'value'
+        """
         raise NotImplementedError
 
-
+    def prepend(self, resource_name, value):
+        """ This method is used to underlap an image under the current image
+            Have a look at the documentation of the 'load' method for the meanings of 'resource_name' and 'value'
+        """
+        raise NotImplementedError
