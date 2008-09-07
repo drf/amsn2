80a7d50cdca3a3939833ebacc6b5ccf1e0d37260
Boris Faure <billiob@users.sourceforge.net>
2008-09-07 20:54:45 +0000

    add base.Window

--- amsn2/gui/base/__init__.py
+++ amsn2/gui/base/__init__.py
@@ -1,6 +1,7 @@
 from main_loop import *
 from login import *
 from contact_list import *
+from window import *
 from main import *
 from image import *
 from splash import *
--- amsn2/gui/base/main.py
+++ amsn2/gui/base/main.py
@@ -1,27 +1,9 @@
 
 class aMSNMainWindow(object):
-    """ This Interface represents the main window of the application. Everything will be done from here """
+    """ This Interface represents the main window of the application. Everything will be done from here 
+    When the window is shown, it should call: amsn_core.mainWindowShown()
+    When the user wants to close that window, amsn_core.quit() should be called.
+    """
+
     def __init__(self, amsn_core):
-        raise NotImplementedError
-
-    def show(self):
-        """ This launches the main window, creates it, etc..
-        and should notify the core of the main window being created"""
-        raise NotImplementedError
-
-    def hide(self):
-        """ This should hide the main window, creates it, etc..
-        and should notify the core of the main window being created"""
-        raise NotImplementedError
-
-    def setTitle(self, text):
-        """ This will allow the core to change the current window's title
-        @text : a string
-        """
-        raise NotImplementedError
-    
-    def setMenu(self, menu):
-        """ This will allow the core to change the current window's main menu
-        @menu : a MenuView
-        """
-        raise NotImplementedError
+        pass
