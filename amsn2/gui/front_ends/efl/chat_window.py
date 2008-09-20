2e19c2d8214f7765bad7c9c537fa7b4051ad29bb
Boris Faure <billiob@users.sourceforge.net>
2008-09-20 15:35:26 +0000

    [EFL] update chat_window.py

--- amsn2/gui/front_ends/efl/chat_window.py
+++ amsn2/gui/front_ends/efl/chat_window.py
@@ -13,8 +13,25 @@ class aMSNChatWindow(window.aMSNWindow, base.aMSNChatWindow):
     def __init__(self, conversation_manager):     
         self._conversation_manager = conversation_manager
         window.aMSNWindow.__init__(self, conversation_manager._core)
+        self._container = aMSNChatWidgetContainer()
         self.setTitle("aMSN - Chatwindow")
 
-
+    def addChatWidget(self, chat_widget):
+        #TODO: use the container
+        self.child = chat_widget
 
 #TODO: ChatWidgetContainer
+
+
+class aMSNChatWidgetContainer:
+    pass
+
+class aMSNChatWidget(etk.Widget, base.aMSNChatWidget):
+    def __init__(self, amsn_conversation, parent):
+        self._parent = parent
+        self._amsn_conversation = amsn_conversation
+        #TODO
+
+
+    def onMessageReceived(self, sender, message):
+        pass
