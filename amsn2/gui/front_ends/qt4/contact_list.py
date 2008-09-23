8a4d765163e4d0c3e332ff9ebc31f7b8158a6636
Dario Freddi <drf54321@gmail.com>
2008-09-23 20:53:24 +0000

    Updating to splitted view, still not working obviously

--- amsn2/gui/front_ends/qt4/contact_list.py
+++ amsn2/gui/front_ends/qt4/contact_list.py
@@ -4,24 +4,51 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from ui_contactlist import Ui_ContactList
 from styledwidget import StyledWidget
+from amsn2.core.views import StringView
+from amsn2.gui import base
 
-class aMSNContactListWindow(StyledWidget, base.aMSNContactListWindow):
+class aMSNContactListWindow(base.aMSNContactListWindow):
+    def __init__(self, amsn_core, parent):
+        self._amsn_core = amsn_core
+        self._parent = parent
+        self._clwidget = aMSNContactListWidget(amsn_core, self)
+        self._clwidget.show()
+
+    def show(self):
+        self._clwidget.show()
+    
+    def hide(self):
+        self._clwidget.hide()
+
+    def setTitle(self, text):
+        self._parent.setTitle(text)
+
+    def setMenu(self, menu):
+        self._parent.setMenu(menu)
+
+    def topCLUpdated(self, contactView):
+        pass #TODO
+
+    
+            
+class aMSNContactListWidget(StyledWidget, base.aMSNContactListWidget):
     def __init__(self, amsn_core, parent):
         """ Should we consider switching the contact view to a Model+View
         instead of a QTreeWidget? This can surely give us some advantages... """
-        StyledWidget.__init__(self, parent)
+        StyledWidget.__init__(self, parent._parent)
         self._amsn_core = amsn_core
         self.ui = Ui_ContactList()
         self.ui.setupUi(self)
         self._parent = parent
+        self._mainWindow = parent._parent
         self.ui.cList.clear()
 
     def show(self):
-        self._parent.fadeIn(self)
+        self._mainWindow.fadeIn(self)
 
     def hide(self):
         pass
-
+    
     def groupAdded(self, group):
         groupItem = QTreeWidgetItem(self.ui.cList)
         groupItem.setText(0, group.name.toString())
