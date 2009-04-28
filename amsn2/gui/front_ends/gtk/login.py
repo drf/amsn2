f3e15f7f96d013eadc421703b9bbe5df2871cd4c
Luca Dariz <luca@descartes.(none)>
2009-04-28 23:53:31 +0800

    [Gtk] Allow setting username and password from command line
    Signed-off-by: jonte <jonatan.p@gmail.com>

--- amsn2/gui/front_ends/gtk/login.py
+++ amsn2/gui/front_ends/gtk/login.py
@@ -191,9 +191,9 @@ class aMSNLoginWindow(gtk.VBox):
         self.current_profile = profile
         if profile is not None:
             self._username = self.current_profile.username
+            self.user.get_children()[0].set_text(self._username)
             self._password = self.current_profile.password
-            
-            
+            self.password.set_text(self._password)
 
     def signin(self):
         self.current_profile.username = self.user.get_active_text()
