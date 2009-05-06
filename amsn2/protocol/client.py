9be17eba206731729ef67d3f89d0e1ee981bdb80
Stéphane Bisinger <stephane.bisinger@gmail.com>
2009-05-06 22:02:09 +0100

    Refactored some names in the protocol module
    Refactored some file names in the protocol module, so that it better
    reflects the class names inside the file. All the events have been moved
    to a subdirectory and all references have been updated.
    
    Signed-off-by: Stéphane Bisinger <stephane.bisinger@gmail.com>

--- amsn2/protocol/client.py
+++ amsn2/protocol/client.py
@@ -1,23 +1,51 @@
+# -*- coding: utf-8 -*-
+#
+# amsn - a python client for the WLM Network
+#
+# Copyright (C) 2008 Dario Freddi <drf54321@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 import papyon
 import papyon.event
+from events.client import *
+from events.contact import *
+from events.invite import *
+from events.oim import *
+from events.addressbook import *
 
-
-class ClientEvents(papyon.event.ClientEventInterface):
-    def __init__(self, client, amsn_core):
+class Client(papyon.Client):
+    def __init__(self, amsn_core, profile):
+        self._amsn_profile = profile
         self._amsn_core = amsn_core
-        papyon.event.ClientEventInterface.__init__(self, client)
+        server = (self._amsn_profile.getConfigKey("ns_server", "messenger.hotmail.com"),
+                  self._amsn_profile.getConfigKey("ns_port", 1863))
+        papyon.Client.__init__(self, server)
 
-    def on_client_state_changed(self, state):
-        self._amsn_core.connectionStateChanged(self._client._amsn_profile, state)
-        
-        if state == papyon.event.ClientState.OPEN:
-            self._client.profile.display_name = "aMSN2"
-            self._client.profile.presence = papyon.Presence.ONLINE
-            self._client.profile.current_media = ("I listen to", "Nothing")
-            self._client.profile.personal_message = "Testing aMSN2!"
+        self._client_events_handler = ClientEvents(self, self._amsn_core)
+        self._contact_events_handler = ContactEvents(self, self._amsn_core._contactlist_manager)
+        self._invite_events_handler = InviteEvents(self, self._amsn_core)
+        self._oim_events_handler = OIMEvents(self, self._amsn_core._oim_manager)
+        self._addressbook_events_handler = AddressBookEvents(self, self._amsn_core)
 
-    def on_client_error(self, error_type, error):
-        print "ERROR :", error_type, " ->", error
-    
-    
+    def connect(self):
+        self.login(self._amsn_profile.email, self._amsn_profile.password)
+
+    def changeNick(self, nick):
+        self.profile.display_name = nick.toString()
+
+    def changeMessage(self, message):
+        self.profile.personal_message = message.toString()
+
