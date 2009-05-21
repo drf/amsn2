683b66a57e61e3df2b1c0b2b295cb4a25a65b7cd
Luca Dariz <luca.dariz@gmail.com>
2009-05-21 14:25:49 +0200

    [core] Added correct DP request [protocol] Added managing of contact changes

--- amsn2/protocol/events/contact.py
+++ amsn2/protocol/events/contact.py
@@ -9,5 +9,28 @@ class ContactEvents(papyon.event.ContactEventInterface):
         papyon.event.ContactEventInterface.__init__(self, client)
 
     def on_contact_presence_changed(self, contact):
-        self._contact_manager.onContactPresenceChanged(contact)
+        self._contact_manager.onContactChanged(contact)
+
+    def on_contact_display_name_changed(self, contact):
+        self._contact_manager.onContactChanged(contact)
+
+    def on_contact_personal_message_changed(self, contact):
+        self._contact_manager.onContactChanged(contact)
+
+    def on_contact_current_media_changed(self, contact):
+        self._contact_manager.onContactChanged(contact)
+
+    def on_contact_msn_object_changed(self, contact):
+        # TODO: filter DPs
+        if contact.msn_object._type is papyon.p2p.MSNObjectType.DISPLAY_PICTURE:
+            self._contact_manager.onContactDPChanged(contact)
+
+    def on_contact_memberships_changed(self, contact):
+        pass
+
+    def on_contact_infos_changed(self, contact, infos):
+        pass
+
+    def on_contact_client_capabilities_changed(self, contact):
+        pass
 
