9c5843a80dfd5276b3cecbd8bb87661424ee9804
Pablo Castellano <pablog.ubuntu@gmail.com>
2009-06-06 12:01:52 +0200

    generateFiles.sh checks if pyuic4 is installed

--- amsn2/gui/front_ends/qt4/generateFiles.sh
+++ amsn2/gui/front_ends/qt4/generateFiles.sh
@@ -2,6 +2,12 @@
 
 #pyrcc4 -o src/faigaresource_rc.py ui/faigaresource.qrc
 
+which pyuic4>/dev/null
+if [ $? != 0 ]; then
+ echo >&2 "This script requires pyuic4 installed."
+ exit 255
+fi
+
 pyuic4 -o ui_login.py login.ui
 pyuic4 -o ui_contactlist.py contactlist.ui
 pyuic4 -o ui_chatWindow.py chatWindow.ui
