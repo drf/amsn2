a60136518340d3ea48d3586274699b92033eb4ce
Luca Dariz <luca.dariz@gmail.com>
2009-07-22 18:51:48 +0200

    Added a base class for the input of contact/group info

---
+++ amsn2/gui/base/input.py
@@ -0,0 +1,17 @@
+
+class aMSNInputWindow(object): 
+    """
+    This Interface represent a window used to get an input,
+    like a new contact or a new group.
+    """
+    def __init__(self, message, type, callback, params):
+        """
+        @type message: str
+        @type type: ContactView or GroupView
+        @param type: contains the view to fill.
+        @type callback: function
+        @param callback: The function that will be called when the view has been filled.
+        The prototype is callback(view), where view is the ContactView or the Grouview
+        filled, or None if the input has been canceled.
+        """
+        raise notImplementedError
